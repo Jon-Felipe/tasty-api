6137d651535cc791f40996c81a3263ae294f3246
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-19 17:09:48 +0100

    basic auth routes setup

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+// routers
+const authRouter = require('./routes/auth');
+
+app.use(express.json());
+
+// routes
+app.use('/api/v1/auth', authRouter);
 
 const port = process.env.PORT || 3000;
 

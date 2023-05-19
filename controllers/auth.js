c0b66b15d0325466965d90116f54d968255689ff
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-19 19:58:42 +0100

    register new user and return user json object

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,18 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+
 const register = async (req, res) => {
-  res.send('register user');
+  const user = await User.create({ ...req.body });
+  res
+    .status(StatusCodes.CREATED)
+    .json({
+      user: {
+        name: user.name,
+        lastName: user.lastName,
+        email: user.email,
+        password: user.password,
+      },
+    });
 };
 
 const login = async (req, res) => {

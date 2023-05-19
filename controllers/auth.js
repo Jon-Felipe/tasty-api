18f7a99794f5ccae6cf922968620102e14389b23
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-19 20:19:48 +0100

    generating and storing token

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,13 @@ const { StatusCodes } = require('http-status-codes');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
+  const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({
     user: {
       name: user.name,
       lastName: user.lastName,
       email: user.email,
+      token,
     },
   });
 };

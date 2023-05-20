369cd35ff3534f519ac6676e9318365f0660d52f
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-20 09:59:48 +0100

    login user functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,32 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new Error('Please provide an email and a password');
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('Invalid Credentials');
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new Error('Invalid Credentials');
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      token,
+    },
+  });
 };
 
 module.exports = { register, login };

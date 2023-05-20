c3ac604597be3bcba780e9857de2ab6fefe18705
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-20 21:58:41 +0100

    added authentication to recipe routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
 
 // routers
 const authRouter = require('./routes/auth');
@@ -18,7 +19,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/recipes', recipesRouter);
+app.use('/api/v1/recipes', authenticateUser, recipesRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);

b9856b1cb082d41a40efeb64107af86d455d8a6c
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-22 13:14:10 +0100

    added more fields to recipe model

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,12 +1,51 @@
 const mongoose = require('mongoose');
 
-const RecipeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide a recipe name'],
-    trim: true,
-    maxLength: [100, 'Recipe name can not be more than 100 characters'],
+const RecipeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide a recipe name'],
+      trim: true,
+      maxLength: [100, 'Recipe name can not be more than 100 characters'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide a description'],
+      maxLength: [1000, 'Description can not be more than 1000 characters'],
+    },
+    image: {
+      type: String,
+      default: '/uploads/example.jpeg',
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    ingredients: {
+      type: [String],
+      required: [true, 'Please provide recipe ingredients'],
+    },
+    directions: {
+      type: [String],
+      required: [true, 'Please provide recipe directions'],
+    },
+    equipment: {
+      type: [String],
+    },
+    recipeTips: {
+      type: [String],
+    },
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide a user'],
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Recipe', RecipeSchema);

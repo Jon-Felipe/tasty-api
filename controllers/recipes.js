406e963f6c13e98bb9ac93196cc03c8d20dab2a1
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-22 20:38:29 +0100

    created update, delete, create recipe basic routes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -6,4 +6,22 @@ const getRecipe = async (req, res) => {
   res.send('getRecipe');
 };
 
-module.exports = { getAllRecipes, getRecipe };
+const createRecipe = async (req, res) => {
+  res.send('create recipe');
+};
+
+const updateRecipe = async (req, res) => {
+  res.send('update recipe');
+};
+
+const deleteRecipe = async (req, res) => {
+  res.send('delete recipe');
+};
+
+module.exports = {
+  getAllRecipes,
+  getRecipe,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe,
+};

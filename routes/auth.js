96a51cf37a0417ffd0b2db2c936f4eae1685022d
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-27 13:50:14 +0100

    changed to patch request

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,6 @@ const { register, login, updateUser } = require('../controllers/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.put('/updateUser', authenticateUser, updateUser);
+router.patch('/updateUser', authenticateUser, updateUser);
 
 module.exports = router;

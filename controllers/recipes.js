faf52a6622af003ec5e884578fd392d6880ffefd
Jon-Felipe <rodriguesjf7@gmail.com>
2023-08-06 11:56:09 +0100

    set limit back to hard coded value 9

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -35,15 +35,15 @@ const getAllRecipes = async (req, res) => {
     result = result.sort('-name');
   }
 
-  const totalRecipes = await Recipe.countDocuments(queryObject);
-
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || totalRecipes;
+  const limit = Number(req.query.limit) || 9;
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
 
   const recipes = await result;
+
+  const totalRecipes = await Recipe.countDocuments(queryObject);
   const numOfPages = Math.ceil(totalRecipes / limit);
 
   res

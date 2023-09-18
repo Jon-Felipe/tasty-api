f07eceaac07d80e590a98971ed5d211a7c426e8a
Jon-Felipe <rodriguesjf7@gmail.com>
2023-09-18 08:45:40 +0100

    get user recipes route updates

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -70,41 +70,14 @@ const getUserRecipes = async (req, res) => {
   const {
     user: { userId },
   } = req;
-  const { search, sort } = req.query;
 
-  let queryObject = { createdBy: userId };
+  const recipes = await Recipe.find({ createdBy: userId }).populate(
+    'createdBy',
+    'name lastName'
+  );
+  const totalRecipes = await Recipe.countDocuments({ createdBy: userId });
 
-  if (search) {
-    queryObject.name = { $regex: search, $options: 'i' };
-  }
-
-  let result = Recipe.find(queryObject).populate('createdBy', 'name lastName');
-
-  if (sort === 'latest') {
-    result = result.sort('-createdAt');
-  }
-  if (sort === 'oldest') {
-    result = result.sort('createdAt');
-  }
-  if (sort === 'a-z') {
-    result = result.sort('name');
-  }
-  if (sort === 'z-a') {
-    result = result.sort('-name');
-  }
-
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 6;
-  const skip = (page - 1) * limit;
-
-  result = result.skip(skip).limit(limit);
-
-  const recipes = await result;
-
-  const totalRecipes = await Recipe.countDocuments(queryObject);
-  const numOfPages = Math.ceil(totalRecipes / limit);
-
-  res.status(StatusCodes.OK).json({ recipes: recipes, numOfPages });
+  res.status(StatusCodes.OK).json({ recipes, totalRecipes });
 };
 
 const createRecipe = async (req, res) => {
